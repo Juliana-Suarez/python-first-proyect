d12ec8efd4fc5889060f918fbf6cd973d429e605
Juliana-Suarez <95254173+Juliana-Suarez@users.noreply.github.com>
2021-12-06 07:47:04 -0500

    Imprint float and string

---
+++ main.py
@@ -0,0 +1,14 @@
+number_1 = 2
+number_2 = 6
+sum_1= number_1+number_2
+print(sum_1)
+
+float_var1= 23.454
+float_var2= 2.54
+float_var = float_var1 + float_var2
+print(float_var)
+
+word_1 = "Buenos "
+word_2 = "Dias"
+word_union= word_1 + word_2
+print(word_union)
