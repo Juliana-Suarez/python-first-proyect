a4a397e812e2ba5235eec728799dda4287d065de
Juliana Suarez <juliana.suarez02@uptc.edu.co>
2021-12-06 15:43:44 -0500

    MODULOS

--- Homework_calculator/main.py1.py
+++ Homework_calculator/main.py1.py
@@ -1,41 +1,62 @@
-print("Bienvenido a la CALCULADORA ")
-print("Esta en una calculadora de operacion basicas")
-print("que trabaja con operaciones de maximo dos valores")
 
-print("1.SUMA")
-print("2.RESTA")
-print("3.MULTIPLICACION")
-print("4.DIVISION")
+def print_welcome_message():
+    print("Bienvenido a la CALCULADORA ")
+    print("Esta en una calculadora de operacion basicas")
+    print("que trabaja con operaciones de maximo dos valores")
 
-opcion = input('Escoja la operacion que desea hacer: ')
+def selection_menu():
+    print("1.SUMA")
+    print("2.RESTA")
+    print("3.MULTIPLICACION")
+    print("4.DIVISION")
+    print("Ingrese una opcion del menu --> ")
+    return int(input())
 
-if opcion == '1' :
-    number_1 = int(input("Ingrese valor 1 --->"))
-    number_2 = int(input("Ingrese valor 2 --->"))
-    sum = number_1+number_2
-    print("Resultado es ---> ", sum  )
+print_welcome_message()
+option=selection_menu()
 
-elif opcion == '2' :
-    number_1 = int(input("Ingrese valor 1 --->"))
-    number_2 = int(input("Ingrese valor 2 --->"))
-    rest = number_1 - number_2
-    print("Resultado es ---> ", rest)
+def numbers_selections1():
+    print("Ingrese valor 1 --->")
+    return int(input())
 
-elif opcion == '3' :
-    number_1 = int(input("Ingrese valor 1 --->"))
-    number_2 = int(input("Ingrese valor 2 --->"))
-    multiplication = number_1 * number_2
-    print("Resultado es ---> ", multiplication)
+def numbers_selections2():
+    print("Ingrese valor 2 --->")
+    return int(input())
 
-else :
-    number_1 = int(input("Ingrese valor 1 --->"))
-    number_2 = int(input("Ingrese valor 2 --->"))
+number_1 = numbers_selections1()
+number_2 = numbers_selections2()
+
+result=0.0
+
+def option_sum(number_1,number_2):
+    result = number_1 + number_2
+    print("Resultado ---> ", result)
+
+
+def option_rest(number_1,number_2):
+    result= number_1 - number_2
+    print("Resultado ---> ", result)
+
+def option_multiplication(number_1,number_2):
+    result = number_1 * number_2
+    print("Resultado ---> ", result)
+
+def option_division_(number_1,number_2):
     if number_2 == '0':
         print("Error matematico, la division no es posible ")
     else:
-        division = number_1 / number_2
-        print("Resultado es ---> ", division)
+        result = number_1 / number_2
+        print("Resultado ---> ", result)
 
+if option == 1 :
+    option_sum(number_1, number_2)
 
+elif option == 2 :
+    option_rest(number_1, number_2)
 
+elif option == 3 :
+    option_multiplication(number_1, number_2)
+
+else :
+    option_division_(number_1, number_2)
 
