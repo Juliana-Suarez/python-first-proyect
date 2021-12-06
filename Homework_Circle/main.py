dbc1df13b5895e865b4bdd5121a6b7f3e2a2ec56
Juliana-Suarez <95254173+Juliana-Suarez@users.noreply.github.com>
2021-12-06 07:48:16 -0500

    Add files via upload

---
+++ Homework_Circle/main.py
@@ -0,0 +1,17 @@
+#calcular el area de un circulo
+
+from  math import pi
+
+print ("Hola")
+print("BIENVENIDO A LA CALCULADORA DE AREA DE UN CIRCULO")
+r = float(input('Ingrese el valor del radio del circulo que quiere calcular:  '))
+
+area = pi * r ** 2
+print("El area del circulo es de--->", area)
+
+
+
+
+
+
+
