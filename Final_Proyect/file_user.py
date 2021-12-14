3d7357712bf75f283507f35f1089dfe28e571da3
Juliana Suarez <juliana.suarez02@uptc.edu.co>
2021-12-13 21:49:23 -0500

    files_proyect

---
+++ Final_Proyect/file_user.py
@@ -0,0 +1,10 @@
+def user_1():
+    user= "pablo2040@gmail.com"
+    return user
+
+def password_1():
+    password1= "Maxi_1312"
+    return password1
+
+real_password=password_1()
+real_user=user_1()
